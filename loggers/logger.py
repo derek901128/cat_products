bdfed115e16fc6f059dd65c6a09c1a22966ad89f
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-10-07 23:50:10 +0800

    Update logger.py

--- loggers/logger.py
+++ loggers/logger.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def set_logger(logger_name: str):
     filename = f"{logger_name}_{datetime.today().strftime('%Y_%b_%d_%H:%M:%S')}.log"
-    project_path = Path('/Users/derek901128/Desktop/programming languages/python/scripts/cats_prod_scraper')
+    project_path = Path('..').resolve()
     loggers_path = project_path.joinpath('loggers')
 
     if not loggers_path.joinpath(logger_name).exists():
